152357e6efd893fd29a34e2e7877359416faa42c
Kazuki Inamura <kzinmr109@gmail.com>
2019-08-20 17:40:58 +0900

    add: beam-search&tag-validation

--- crf_entity_extractor.py
+++ crf_entity_extractor.py
@@ -1,51 +1,15 @@
 from tqdm import tqdm
 
 import pickle
+import multiprocessing
 # from pyknp import KNP
 from sklearn_crfsuite import CRF
 from sklearn_crfsuite import metrics
+import numpy as np
 from typing import List, Dict
 
-LABELS = ['Person',
-'Organization_Other',
-'International_Organization',
-'Show_Organization',
-'Family',
-'Ethnic_Group_Other',
-'Nationality',
-'Sports_Organization_Other',
-'Pro_Sports_Organization',
-'Sports_League',
-'Corporation_Other',
-'Company',
-'Company_Group',
-'Political_Organization_Other',
-'Government',
-'Political_Party',
-'Cabinet',
-'Military',
-'GPE_Other',
-'City',
-'County',
-'Province',
-'Country',
-'GOE_Other',
-'Public_Institution',
-'School',
-'Research_Institute',
-'Market',
-'Park',
-'Sports_Facility',
-'Museum',
-'Zoo',
-'Amusement_Park',
-'Theater',
-'Worship_Place',
-'Car_Stop',
-'Station',
-'Airport',
-'Port',
-'Position_Vocation']
+with open('input_kb/labels_enesub.txt') as f:
+    LABELS = f.read().split('\n')
 
 
 class Sentence:
@@ -78,20 +42,22 @@ class FeatureExtractor:
             "bias": 1.0,
             "word": entry[0],
             "postag": entry[1],
-            # "kb_match": self.__select(entry[2])
+            "chunktag": entry[2],
+            "kb_match": entry[3]
         }
-        for enetype in entry[2].split('##')[:1]:
-            features[enetype] = 1
+        # for enetype in entry[3].split('##')[:1]:
+        #     features[enetype] = 1
         return features
 
     def window_features(self, entry, w):
         features = {
             f"{w}:word": entry[0],
             f"{w}:postag": entry[1],
-            # f"{w}:kb_match": self.__select(entry[2])
+            f"{w}:chunktag": entry[2],
+            f"{w}:kb_match": entry[3]
         }
-        for enetype in entry[2].split('##')[:1]:
-            features[f"{w}:{enetype}"] = 1
+        # for enetype in entry[3].split('##')[:1]:
+        #     features[f"{w}:{enetype}"] = 1
         return features
 
     def extract_feature(self, sentence: Sentence):
@@ -120,6 +86,76 @@ class FeatureExtractor:
 
         return features
 
+def _beam_search_decoder(args):
+    probability, k = args
+    topk_candidates = [(list(), 1.0)]
+    for row in probability:
+        topk_candidates = sorted([(seq + [i], score * -np.log(prob))  # NOTE: prob==0.?
+                                    for seq, score in topk_candidates
+                                    for i, prob in enumerate(row)],
+                                    key=lambda x: x[1])[:k]
+    return topk_candidates
+
+def _is_valid(labels):
+    prev_bio, prev_netype, netype = '', '', ''
+    for l in labels:
+        if len(l.split('-')) == 2:
+            bio, netype = l.split('-')
+        else:
+            bio = l
+        # check two bad patterns like ['O', 'I-A', 'O'] or ['B-A', 'I-B', 'O']
+        if prev_bio == 'O' and bio == 'I' or prev_bio == 'B' and bio == 'I' and prev_netype != netype:
+            return False
+        prev_bio = bio
+        prev_netype = netype
+    return True
+
+def _filter_valid(args):
+    for labels in args:
+        if _is_valid(labels):
+            return labels
+    return args[0]
+
+def _remove_invalid_labels(labels):
+    if _is_valid(labels):
+        return labels
+    prev_bio, prev_netype, netype = '', '', ''
+    i = 0
+    remove_indices = []
+    while i < len(labels):
+        l = labels[i]
+        if len(l.split('-')) == 2:
+            bio, netype = l.split('-')
+        else:
+            bio = l
+        # check two bad patterns like ['O', 'I-A', 'O'] or ['B-A', 'I-B', 'O']
+        if prev_bio == 'O' and bio == 'I':
+            remove_from = i
+            j = i + 1
+            while labels[j].split('-')[0] != 'O':
+                j += 1
+            remove_to = j
+            remove_indices.append((remove_from, remove_to))
+            i = j
+        elif prev_bio == 'B' and bio == 'I' and prev_netype != netype:
+            remove_from = i
+            j = i + 1
+            while labels[j].split('-')[0] == 'I':
+                j += 1
+            remove_to = j
+            remove_indices.append((remove_from, remove_to))
+            i = j
+        else:
+            i += 1
+        prev_bio = bio
+        prev_netype = netype
+
+    for (rm_from, rm_to) in remove_indices:
+        labels[rm_from: rm_to] = ['[NULL]' for _ in range(rm_to - rm_from)]
+    return labels
+
+def _fix_valid(args):
+    return [_remove_invalid_labels(labels) if _is_valid(labels) else labels for labels in args]
 
 class EntityExtractor:
 
@@ -181,24 +217,74 @@ class EntityExtractor:
         y = [s.labels for s in sentences]
         self.crf.fit(x, y)
 
-    def evaluate(self, sentences: List[Sentence]):
+    def evaluate(self, sentences: List[Sentence], fix_invalid_labels=True, decoder='greedy', k=5, debug=False):
         """
         return predicted class
         """
         x = [self.fe.extract_feature(s) for s in sentences]
         y_test = [s.labels for s in sentences]
-        y_pred = self.crf.predict(x)
+        y_pred = self.__predict(x, decoder, k)
+        if debug:
+            for t, p in list(zip(y_test, y_pred))[:10]:
+                print(t)
+                print(p)
+
+        print('raw, ', decoder)
         print(metrics.flat_classification_report(
             y_test, y_pred, labels=sorted(LABELS), digits=3
         ))        
+        if fix_invalid_labels:
+            print('fix-invalid, ', decoder)
+            y_pred = self.fix_labels(y_pred)
+            if debug:
+                for t, p in list(zip(y_test, y_pred))[:10]:
+                    print(t)
+                    print(p)
+            print(metrics.flat_classification_report(
+                y_test, y_pred, labels=sorted(LABELS), digits=3
+            ))
         return metrics.flat_f1_score(y_test, y_pred, average='weighted', labels=LABELS)
 
-    def predict(self, sentences: List[Sentence]):
+    def fix_labels(self, labels):
+        labels_fixed = []
+        with multiprocessing.Pool() as p:
+            labels_fixed.append(p.map(_fix_valid, labels))
+        return labels_fixed[0]
+
+    def __predict(self, x, decoder='greedy', k=5):
+        if decoder == 'greedy':
+            return self.crf.predict(x)
+        else:
+            y_probs = self.__predict_prob(x)
+            # (n_data, max_sequence_length, n_labels) -> (n_data, k, max_sequence_length)
+            args = [(probability, k) for probability in y_probs]
+            results = []
+            with multiprocessing.Pool(multiprocessing.cpu_count()) as p:
+                results.append(p.map(_beam_search_decoder, args))
+            beams_list = results[0]
+
+            # validのみ残す
+            args = [[[LABELS[idx-1] if idx > 0 else 'O' for idx in beam[0]] for beam in beams] for beams in beams_list]
+            labels_selected = []
+            with multiprocessing.Pool(multiprocessing.cpu_count()) as p:
+                labels_selected.append(p.map(_filter_valid, args))
+                labels_selected = labels_selected[0]
+
+            return labels_selected
+
+    def predict(self, sentences: List[Sentence], decoder='greedy', k=5):
         """
         return predicted class
         """
         x = [self.fe.extract_feature(s) for s in sentences]
-        return self.crf.predict(x)
+        return self.__predict(x, decoder, k)
+
+    def __predict_prob(self, x):
+        probs = self.crf.predict_marginals(x)
+        # (n_data, max_sequence_length, n_labels)
+        return [[[p_token['O']] + [p_token[label] for label in LABELS]
+                 for p_token in prob]
+                for prob in probs]
 
     def predict_prob(self, sentences: List[Sentence]):
         """
@@ -207,28 +293,32 @@ class EntityExtractor:
         :return:
         """
         x = [self.fe.extract_feature(s) for s in sentences]
-        return self.crf.predict_marginals(x)
+        return self.__predict_prob(x)
+        
 
 
 def read_data(filepath):
     with open(filepath) as f:
         return [Sentence(sentence_str) for sentence_str in f.read().split('\n\n')]
 
-
 if __name__ == "__main__":
-    model_path = 'crf_gazetter_single.pkl'
+    model_path = 'crf_gazetter_multi.pkl'
 
-    train_path = 'NERdata_ene_single_cv_5/NERdata_0/train.txt'
-    sentences_train = read_data(train_path)
-    print('data loaded')
-    ee = EntityExtractor()
-    ee.train(sentences_train)
-    ee.save_model(model_path)
+    train = False
+    if train:
+        train_path = 'input_kb/train.txt'  # 'NERdata_ene_single_cv_5/NERdata_0/train.txt'
+        sentences_train = read_data(train_path)
+        print('data loaded')
+        ee = EntityExtractor()
+        ee.train(sentences_train)
+        ee.save_model(model_path)
 
-    test_path = 'NERdata_ene_single_cv_5/NERdata_0/test.txt'
+    test_path = 'input_kb/test.txt'  # 'NERdata_ene_single_cv_5/NERdata_0/test.txt'
     sentences_test = read_data(test_path)
     ee = EntityExtractor()
     ee.load_model(model_path)
-    result = ee.predict(sentences_test)
-    with open('crfresult_test_gazetter_single.pkl', 'wb') as f:
-        pickle.dump(result, f)
+    ee.evaluate(sentences_test)
+    ee.evaluate(sentences_test, decoder='beam')
+    # result = ee.predict(sentences_test, decoder='greedy')
+    # with open('crfresult_test_gazetter_multi.pkl', 'wb') as f:
+    #     pickle.dump(result, f)
